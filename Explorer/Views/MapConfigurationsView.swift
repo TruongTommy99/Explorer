75d760975c7e54c102193381b6d52df51f42471a
TommyTruongPDL <122547301+TommyTruongPDL@users.noreply.github.com>
2023-04-15 12:58:37 -0400

    added weather information

--- Explorer/Views/MapConfigurationsView.swift
+++ Explorer/Views/MapConfigurationsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MapConfigurationsView: View {
     @EnvironmentObject var mapConfigurations: MapConfigurations
-    
+        
     var body: some View {
         VStack {
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
